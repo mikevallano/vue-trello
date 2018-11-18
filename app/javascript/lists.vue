069930dd385dc2eb4d598318bbcaf0acfc6da10d
mike vallano <mikevallano@gmail.com>
2018-11-18 10:26:18 -0600

    Add and render cards with vue.

--- app/javascript/lists.vue
+++ app/javascript/lists.vue
@@ -2,12 +2,15 @@
   <div> <!-- you can only return one item, so wrapping in a div (fragment not supported) -->
     <p>{{ componentMessage }}</p>
     <div class="row">
-      <div class='col-3' v-for="list in original_lists">
+      <div class='col-3' v-for="list in lists">
         <h4>{{list.name}}</h4>
 
-        <div v-for="(card, index) in list.cards" class="card card-body">
+        <div v-for="(card, index) in list.cards" class="card card-body mb-3">
           {{card.name}}
         </div>
+
+        <textarea v-model="newCards[list.id]" class='form-control'></textarea>
+        <button v-on:click="saveCard(list.id)" class='btn btn-primary btn-sm'>Save</button>
       </div>
     </div>
   </div>
@@ -18,7 +21,28 @@ export default {
   props: ["original_lists"], // props needs to be separate from data
   data: function () {
     return {
+      newCards: {},
+      lists: this.original_lists,
       componentMessage: "Lists from vue component"
+    }
+  },
+  methods: {
+    saveCard: function (list_id){
+      let data = new FormData
+      data.append("card[list_id]", list_id)
+      data.append("card[name]", this.newCards[list_id])
+
+      Rails.ajax({
+        url: '/cards',
+        type: 'POST',
+        data: data,
+        dataType: 'json',
+        success: (data) => {
+          let index = this.lists.findIndex(item => item.id == list_id)
+          this.lists[index].cards.push(data)
+          this.newCards[list_id] = '' // clear the data for this card
+        }
+      })
     }
   }
 }
