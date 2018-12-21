46b2d0532178b5804a68e3e11b95b82d4e4e617a
mike vallano <mikevallano@gmail.com>
2018-12-20 19:55:05 -0600

    Use window storage. Break list component out.

--- app/javascript/lists.vue
+++ app/javascript/lists.vue
@@ -1,24 +1,15 @@
 <template>
   <draggable v-model="lists" :options="{group: 'lists', animation: 1000}" @end="listMoved" class="board dragArea">
 <!--     defining group ensures the scope of what can be dragged. for example, lists can't be dragged to cards because cards are not in the group -->
-    <div class='list' v-for="list in lists">
-      <h4>{{list.name}}</h4>
-
-      <draggable v-model="list.cards" :options="{group: 'cards', animation: 1000}" @change="cardMoved" class="dragArea" >
-        <div v-for="(card, index) in list.cards" class="card card-body bg-light border-secondary mb-3">
-          {{card.name}}
-        </div>
-      </draggable>
-
-      <textarea v-model="newCards[list.id]" class='form-control' placeholder="Add new card to this list"></textarea>
-      <button v-on:click="saveCard(list.id)" class='btn btn-dark btn-sm mt-1 save-card-btn'>Save</button>
-    </div>
+  <list v-for="(list, index) in lists" :list="list"></list>
   </draggable>
 </template>
 
 <script>
 
 import draggable from 'vuedraggable'
+import list from 'components/list'
+
 export default {
   props: ["original_lists"], // props needs to be separate from data
   data: function () {
@@ -28,7 +19,7 @@ export default {
       componentMessage: "Lists from vue component"
     }
   },
-  components: { draggable },
+  components: { draggable, list },
   methods: {
     listMoved: function (event) {
       let data = new FormData
@@ -40,46 +31,6 @@ export default {
         data: data,
         format: 'json'
 
-      })
-    },
-    cardMoved: function (event) {
-      let evt = event.added || event.moved
-      if (evt === undefined) {return}
-
-      let element = evt.element
-
-      let list_index = this.lists.findIndex((list) => {
-        return list.cards.find(function(card) {
-          return card.id === element.id
-        })
-      })
-
-      let data = new FormData
-      data.append("card[list_id]", this.lists[list_index].id)
-      data.append("card[position]", evt.newIndex + 1)
-
-      Rails.ajax({
-        url: `/cards/${element.id}/move`,
-        type: 'PATCH',
-        data: data,
-        format: 'json'
-      })
-    },
-    saveCard: function (list_id){
-      let data = new FormData
-      data.append("card[list_id]", list_id)
-      data.append("card[name]", this.newCards[list_id])
-
-      Rails.ajax({
-        url: '/cards',
-        type: 'POST',
-        data: data,
-        dataType: 'json',
-        success: (data) => {
-          let index = this.lists.findIndex(item => item.id == list_id)
-          this.lists[index].cards.push(data)
-          this.newCards[list_id] = '' // clear the data for this card
-        }
       })
     }
   }
--- app/javascript/packs/lists_vue.js
+++ app/javascript/packs/lists_vue.js
@@ -1,12 +1,15 @@
 import Vue from 'vue/dist/vue.esm'
 import Lists from '../lists.vue'
 
+window.store = {}
+
 document.addEventListener('DOMContentLoaded', () => { //if using turbolinks, you have to do on turbolinks load
   let element = document.getElementById('boards');
+  window.store.lists = JSON.parse(element.dataset.lists)
   const listsVue = new Vue({
     el: element,
     data: {
-      lists: JSON.parse(element.dataset.lists),
+      lists: window.store.lists,
       testmessage: 'holamundo',
     },
     template: "<Lists :original_lists='lists' />", //passes original_lists as a prop to the Lists vue object
